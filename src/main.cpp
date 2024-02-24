ac7bd157b85ded13caf2b6dfa55721a112d98350
matthewqiu01 <matthew.qiu01@gmail.com>
2024-02-24 15:13:25 -0500

    Start hardcode

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,62 @@
 #include <FEHLCD.h>
 #include <iostream>
-#include <source/kernel.hpp>
+#include <FEHIO.h>
+#include <FEHMotor.h>
 
-int i = 0;
-int j = 0;
+#define turn_constant 0.5
 
-int main(void) {
-    LCD.Clear(BLACK);
-    LCD.WriteLine("Hello, World!");
-	return 0;
+FEHMotor left_motor(FEHMotor::Motor0, );
+FEHMotor right_motor(FEHMotor::Motor1, );
+AnalogInputPin bump(FEHIO::P0_0);
+
+void DriveStraight(int, float); //1 for forward, -1 for backward. float for seconds
+void DriveTurn(int, float); //1 for right, -1 for left. float for seconds
+
+void DriveStraight(int direction, float distance)
+{
+    left_motor.SetPercent((direction * 25));
+    right_motor.SetPercent((direction * 25));
+
+    if(distance != 0)
+    {
+       Sleep(distance); 
+    }
+    else
+    {
+        while(bump.Value() == 1)
+        {}
+    }
+
+    left_motor.SetPercent(0);
+    right_motor.SetPercent(0);
+    Sleep(1.0);
 }
+
+void DriveTurn(int direction, float angle)
+{
+    left_motor.SetPercent((direction * 15));
+    right_motor.SetPercent((direction * -15));
+    Sleep(angle);
+
+    left_motor.SetPercent(0);
+    right_motor.SetPercent(0);
+    Sleep(1.0);
+}
+
+
+int main(void) 
+{
+    DriveTurn(1, 0.5); //realign
+    DriveStraight(1, 3.5); //drive up the ramp
+    DriveTurn(-1, turn_constant); //turn left
+    DriveStraight(1, 0.5); //forward a bit
+    DriveTurn(1, turn_constant); //turn right
+    DriveStraight(1, 0); //drive into buttons
+    DriveStraight(-1, 2); //drive backwards
+    DriveTurn(1, turn_constant); //turn
+    DriveStraight(1, .5); //forward a bit
+    DriveTurn(1, turn_constant); //turn
+    DriveStraight(1, 0); //drive down the ramp
+}
+
+
