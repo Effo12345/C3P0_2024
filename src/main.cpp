debc333d758c1f1bbfe2bea7c709298569187d52
Effo12345 <4p7septipuses@gmail.com>
2024-03-12 02:17:26 -0400

    Removed test cases from main TODO: Separate route into autonomous.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 #include <FEHSD.h>
 #include <FEHBuzzer.h>
 #include <FEHBattery.h>
+#include <starlib/display/interface.hpp>
 
 AnalogInputPin cds(FEHIO::P3_7);
 
@@ -19,87 +20,19 @@ AnalogInputPin cds(FEHIO::P3_7);
 */
 
 int main(void) {
-    LCD.Clear(BLACK);
-    //LCD.WriteLine("Hello, World!");
+    std::shared_ptr<starlib::Interface> gui = std::make_shared<starlib::Interface>();
+    gui->init();
 
+    int tmpX, tmpY;
+    while(!LCD.Touch(&tmpX, &tmpY)) {
+        gui->update();
+        Sleep(10);
+    }
 
-    /*
-        Odom test
-    */
-//    starlib::Odom odometer;
-//    odometer.withSensors({FEHIO::P0_0, FEHIO::P0_1, 2.5f}, {FEHIO::P1_0, FEHIO::P1_1, 2.5f});
-//    odometer.withOffsets(-2.715f, 2.715f);
-
-//     driveL.SetPercent(100);
-//     driveR.SetPercent(100);
-
-//     starlib::Odom::Pose pos = odometer.getPos();
-//     odometer.tareWheelVelocity();
-//    while(odometer.getPos().p.y < 96) {
-//         odometer.step();
-//         pos = odometer.getPos();
-//         LCD.WriteAt(pos.p.x, 0, 0);
-//         LCD.WriteAt(pos.p.y, 100, 0);
-//         LCD.WriteAt(pos.a, 200, 0);
-
-//         starlib::Odom::Velocity vel = odometer.getVel();
-
-//         Sleep(10);
-//    }
-
-//     LCD.WriteAt(pos.p.x, 0, 0);
-//     LCD.WriteAt(pos.p.y, 100, 0);
-//     LCD.WriteAt(pos.a, 200, 0);
-
-//     driveL.SetPercent(0);
-//     driveR.SetPercent(0);
-
-
-    /*  
-        Pure pursuit test
-    */
-    // starlib::Odom odometer;
-    // odometer.withSensors({FEHIO::P0_0, FEHIO::P0_1, 2.5f}, {FEHIO::P1_0, FEHIO::P1_1, 2.5f});
-    // odometer.withOffsets(-2.715f, 2.715f);
-    // odometer.setPos({{0, -35.961439836448605}, 0.0f});
-
-    // starlib::Wayfinder pather;
-
-    // FEHFile* targetVel = SD.FOpen("target.txt", "w");
-    // FEHFile* measuredVel = SD.FOpen("measure.txt", "w");
-    // LCD.Clear(BLACK);
-
-    // starlib::Odom::Velocity vel = odometer.getVel();
-    // starlib::Odom::Pose pos = odometer.getPos();
-
-    // while(std::fabs(vel.leftVel) > 2.0f || pos.p.distanceTo({0, -35.961439836448605}) < 2.0f) {
-    //     odometer.step();
-    //     pos = odometer.getPos();
-    //     LCD.WriteAt(pos.p.x, 0, 0);
-    //     LCD.WriteAt(pos.p.y, 100, 0);
-    //     LCD.WriteAt(pos.a, 200, 0);
-
-    //     vel = odometer.getVel();
-    //     LCD.WriteAt(vel.leftVel, 0, 20);
-    //     LCD.WriteAt(vel.rightVel, 100, 20);
-
-    //     starlib::Odom::Velocity motorPwr = pather.step(pos, vel);
-    //     driveL.SetPercent(starlib::clamp(motorPwr.leftVel, -100.0f, 100.0f));
-    //     driveR.SetPercent(starlib::clamp(motorPwr.rightVel, -100.0f, 100.0f));
-
-    //     SD.FPrintf(targetVel, "%f\n", (motorPwr.leftVel / 100.0f) * 200.0f);
-    //     SD.FPrintf(measuredVel, "%f\n", vel.leftVel);
-
-    //     Sleep(10);
-    // }
-
-    // driveL.Stop();
-    // driveR.Stop();
-
-    // SD.FClose(targetVel);
-    // SD.FClose(measuredVel);
-
-    // Buzzer.Beep();
+    while(true) {
+        gui->update(true);
+        Sleep(10);
+    }
 
     starlib::Chassis chassis(FEHMotor::Motor1, FEHMotor::Motor0, 9.0f, 
                             {FEHIO::P0_0, FEHIO::P0_1}, 2.5f, -2.8755f,
@@ -387,93 +320,8 @@ int main(void) {
 
     SD.FClose(posOut);
 
-    // while(true) {
-    //     LCD.WriteAt(cds.Value(), 0, 40);
-    //     Sleep(50);
-    // }
 
 
-    // chassis.turn(180.0f);
-
-    
-
-    /*
-        Encoder test
-    */
-//    QuadEncoder encoder(FEHIO::P0_0, FEHIO::P0_1, 2.5f);
-
-//    while(true) {
-//         driveL.SetPercent(10);
-//         float start = TimeNow();
-//         while(TimeNow() - start < 1) {
-//             int counts = encoder.ticks();
-//             LCD.WriteAt(counts, 0, 0);
-//             Sleep(50);
-//             LCD.Clear(BLACK);
-//         }
-
-//         driveL.SetPercent(0);
-//         Sleep(500);
-
-//         driveL.SetPercent(-10);
-//         start = TimeNow();
-//         while(TimeNow() - start < 1) {
-//             int counts = encoder.ticks();
-//             LCD.WriteAt(counts, 0, 0);
-//             Sleep(50);
-//             LCD.Clear(BLACK);   
-//         } 
-
-//         driveL.SetPercent(0);
-//         Sleep(500);    
-//    }
-
-
-    /*
-        Quad encoder test
-    */
-    // QuadEncoder test(FEHIO::P0_0, FEHIO::P0_1, 2.5f);
-    // QuadEncoder test2(FEHIO::P1_0, FEHIO::P1_1, 2.5f);
-
-    // driveL.SetPercent(10);
-
-    // while(test.degrees() < 360.0f) {
-    //     Sleep(50);
-    // }
-
-    // driveL.SetPercent(0);
-    // LCD.WriteAt(test.degrees(), 0, 0);
-    // LCD.WriteAt(test.ticks(), 0, 20);
-    // Sleep(500);
-
-    // driveL.SetPercent(-10);
-    // while(test.degrees() > 0.0f) {
-    //     Sleep(500);
-    // }
-
-    // driveL.SetPercent(0);
-    // LCD.WriteAt(test.degrees(), 0, 0);
-    // LCD.WriteAt(test.ticks(), 0, 20);
-
-    // driveL.SetPercent(25);
-    // driveR.SetPercent(25);
-    // QuadEncoder test(FEHIO::P0_0, FEHIO::P0_1, 2.5f);
-    // QuadEncoder test2(FEHIO::P1_0, FEHIO::P1_1, 2.5f);
-
-    // float start = TimeNow();
-    // while(TimeNow() - start < 10.0f) {
-    //     Sleep(50);
-    //     LCD.WriteAt(TimeNow(), 0, 100);
-    // }
-
-    // LCD.WriteAt(test.degrees(), 0, 0);
-    // LCD.WriteAt(test2.degrees(), 150, 0);
-
-    // driveL.SetPercent(0);
-    // driveR.SetPercent(0);
-
-
-    // starlib::Chassis drive;
 
 	return 0;
 }
