4d0098c52e7ac2c787e37080aecdfcd5f3262709
Effo12345 <4p7septipuses@gmail.com>
2024-03-27 20:12:14 -0400

    Unfinished Cds class

--- include/starlib/cds.hpp
+++ include/starlib/cds.hpp
@@ -1,3 +1,28 @@
 #pragma once
 
 #include <FEHIO.h>
+#include <starlib/display/interface.hpp>
+
+namespace starlib {
+
+    class Cds {
+        std::shared_ptr<AnalogInputPin> cds;
+        std::shared_ptr<Interface> gui;
+
+        float ambient;
+        int measuredColor;
+
+        float sample(const int numSamples = 10);
+    public:
+        Cds(const FEHIO::FEHIOPin& pin, const std::shared_ptr<Interface> interface);
+
+        void sampleAmbient();
+        void sampleLight(float redThreshold);
+        void awaitStartingLight(float threshold);
+
+
+        int getColor();
+        float getOffsetValue();
+    };
+
+}
--- src/starlib/cds.cpp
+++ src/starlib/cds.cpp
@@ -1,1 +1,36 @@
 #include <starlib/cds.hpp>
+
+namespace starlib {
+
+    Cds::Cds(const FEHIO::FEHIOPin& pin, const std::shared_ptr<Interface> interface) {
+        cds = std::make_shared<AnalogInputPin>(pin);
+        gui = interface;
+    }
+
+    float Cds::sample(const int numSamples) {
+        float runningSum {};
+        for(int i = 0; i < numSamples; i++) {
+            runningSum += cds->Value();
+            Sleep(10);
+        }
+    }
+
+    void Cds::sampleAmbient() {
+        const int numSamples = 10;
+        ambient = sample(numSamples) / static_cast<float>(numSamples);
+    }
+
+    void Cds::sampleLight(float redThreshold) {
+        
+    }
+
+    void Cds::awaitStartingLight(float threshold) {
+
+    }
+
+
+    int Cds::getColor() {
+
+    }
+
+}
